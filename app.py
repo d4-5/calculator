2cc20c95483f96ac3b4d58deeb8a92ce9bae7763
d4-5 <111686428+d4-5@users.noreply.github.com>
2023-12-09 15:23:02 +0200

    Intial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from sympy import sympify
+from type1 import type1
+from type2 import type2
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/equation')
+def equation():
+    equation_type = request.args.get('equation_type')
+    return render_template('form.html', equation_type=equation_type)
+
+@app.route('/solve')
+def solve_equation():
+    equation_type = request.args.get('equation_type')
+    try :
+        a = sympify(request.args.get('a'))
+        phi = sympify(request.args.get('phi'))
+        psi = sympify(request.args.get('psi'))
+        mu = sympify(request.args.get('mu'))
+        f = sympify(request.args.get('f'))
+
+        if equation_type == "type1":
+            calculated_equation = type1(a, phi, psi, mu, f)
+        else :
+            calculated_equation = type2(a, phi, psi, mu, f)
+
+        return render_template('show_equation.html', calculated_equation=calculated_equation)
+    except Exception as e:
+        return render_template('form.html', equation_type=equation_type, error=str(e))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ type1.py
@@ -0,0 +1,86 @@
+from sympy import symbols, latex, integrate, simplify
+
+def type1(a, phi, psi, mu, f):
+    x, t, xi, tau = symbols('x t xi tau')
+
+    nu1_I1 = simplify((phi.subs(x, x+a*t)+phi.subs(x, x-a*t))/2)
+    nu1_I2 = simplify(1/(2*a)*integrate(psi.subs(x, xi), (xi, x-a*t,x+a*t)))
+    nu1_I3 = simplify(1/(2*a)*integrate(integrate(f.subs({x: xi, t: tau}), (xi, x-a*(t-tau),x+a*(t-tau))), (tau, 0, t)))
+    nu1 = simplify(nu1_I1 + nu1_I2 + nu1_I3)
+
+    nu2_I1 = simplify((phi.subs(x, x+a*t)+phi.subs(x, a*t-x))/2)
+    nu2_I2 = simplify(1/(2*a)*integrate(psi.subs(x, xi), (xi, a*t-x,x+a*t)))
+    nu2_I3 = simplify(1/(2*a)*(
+        integrate(integrate(f.subs({x: xi, t : tau}), (xi, x-a*(t-tau),x+a*(t-tau))), (tau, t-x/a, t)) +
+        integrate(integrate(f.subs({x: xi, t : tau}), (xi, a*(t-tau)-x,x+a*(t-tau))), (tau, 0, t-x/a))
+    ))
+    nu2 = simplify(nu2_I1 + nu2_I2 + nu2_I3)
+
+    omega = simplify(mu.subs(t, t - x/a))
+    equation = fr"""
+    \begin{{align}}
+    \text{{Умова задачі}}
+    \end{{align}}
+    \begin{{cases}}
+        u_{{tt}}(x, t) &= {latex(a**2)} u_{{xx}}(x, t) + {latex(f)}, \\
+        u(x, 0) &= {latex(phi)}, \ u_t(x, 0) = {latex(psi)}, \\
+        1.\ u(0, t) &= {latex(mu)}.
+    \end{{cases}}
+    
+    
+    \begin{{align}}
+    \text{{I. Неоднорідне диференціальне рівняння}}
+    \end{{align}}
+    \begin{{cases}}
+        \nu_{{tt}}(x, t) &= {latex(a**2)} \nu_{{xx}}(x, t) + {latex(f)}, \\
+        \nu(x, 0) &= {latex(phi)}, \ \nu_t(x, 0) = {latex(psi)}, \\
+        1. \ \nu(0, t) &= 0.
+    \end{{cases}}
+
+    \begin{{align}}
+        \text{{При}}\ t < \frac{{x}}{{a}}, \ x > 0 \\
+    \end{{align}}
+    \begin{{align}}
+    I_1 &= {latex(nu1_I1)} \\
+    I_2 &= {latex(nu1_I2)} \\
+    I_3 &= {latex(nu1_I3)} \\
+    \nu &= {latex(nu1)} \\
+    \end{{align}}
+
+    \begin{{align}}
+        \text{{При}}\ t > \frac{{x}}{{a}}, \ x > 0
+    \end{{align}}
+    \begin{{align}}
+    I_1 &= {latex(nu2_I1)} \\
+    I_2 &= {latex(nu2_I2)} \\
+    I_3 &= {latex(nu2_I3)} \\
+    \nu &= {latex(nu2)} \\
+    \end{{align}}
+
+    \begin{{align}}
+    \text{{II. Однорідне диференціальне рівняння}}
+    \end{{align}}
+    \begin{{cases}}
+        \omega_{{tt}}(x, t) &= {latex(a**2)} \omega_{{xx}}(x, t), \\
+        \omega(x, 0) &= \omega_t(x, 0) = 0, \\
+        1. \ \omega(0, t) &= {latex(mu)}. 
+    \end{{cases}}
+
+    \begin{{align}}
+    \omega(x, t) &=
+    \begin{{cases}}
+        0, & 0 \leqslant t \leqslant \frac{{x}}{{{latex(a)}}}, \\
+        {latex(omega)}, & t > \frac{{x}}{{{latex(a)}}}.
+    \end{{cases}}
+    \end{{align}}
+
+    \begin{{align}}
+    \text{{Відповідь}}
+    \end{{align}}
+    \begin{{align}}
+        \text{{Якщо }} t < \frac{{x}}{{{latex(a)}}}, \ \text{{то }} u(x,t) = {latex(nu1)} \\
+        \text{{Якщо }} t > \frac{{x}}{{{latex(a)}}}, \ \text{{то }} u(x,t) = {latex(simplify(nu2 + omega))}
+    \end{{align}}
+    """
+
+    return equation
